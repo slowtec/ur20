fbf6abaef313a70ef3d455fdf1d35735136c3764
Markus Kohlhase <markus.kohlhase@slowtec.de>
2018-01-06 23:14:04 +0100

    impl Module for UR20-4AO-UI-16

--- src/ur20_4ao_ui_16.rs
+++ src/ur20_4ao_ui_16.rs
@@ -2,9 +2,33 @@
 
 use super::*;
 
+pub struct Mod {
+    pub ch_params: Vec<ChannelParameters>,
+}
+
 #[derive(Debug, Clone)]
 pub struct ChannelParameters {
     pub data_format: DataFormat,
     pub output_range: AnalogUIRange,
-    pub substitute_value: AnalogUIRange
+    pub substitute_value: f32,
+}
+
+impl Default for ChannelParameters {
+    fn default() -> Self {
+        ChannelParameters {
+            data_format: DataFormat::S7,
+            output_range: AnalogUIRange::Disabled,
+            substitute_value: 0.0,
+        }
+    }
+}
+
+impl Module for Mod {
+    fn process_input_word_count(&self) -> usize {
+        0
+    }
+
+    fn process_input(&mut self, _: &[u16]) -> Result<Vec<ChannelValue>, Error> {
+        Ok((0..4).map(|_| ChannelValue::None).collect())
+    }
 }
