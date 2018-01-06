faf5089fb5bcadd632c86b11ecfb3e1d65cbc05b
Markus Kohlhase <markus.kohlhase@slowtec.de>
2018-01-06 23:19:21 +0100

    impl Module for UR20-4DO-P

--- src/ur20_4do_p.rs
+++ src/ur20_4do_p.rs
@@ -1,6 +1,36 @@
 //! Digital output module UR20-4DO-P
 
+use super::*;
+
+pub struct Mod {
+    pub ch_params: Vec<ChannelParameters>,
+}
+
 #[derive(Debug, Clone)]
 pub struct ChannelParameters {
     pub substitute_value: bool,
+}
+
+impl Default for ChannelParameters {
+    fn default() -> Self {
+        ChannelParameters { substitute_value: false }
+    }
+}
+
+impl Default for Mod {
+    fn default() -> Self {
+        let ch_params = (0..4).map(|_| ChannelParameters::default()).collect();
+        Mod { ch_params }
+    }
+}
+
+impl Module for Mod {
+    fn process_input_word_count(&self) -> usize {
+        0
+    }
+
+    fn process_input(&mut self, _: &[u16]) -> Result<Vec<ChannelValue>, Error> {
+        Ok((0..4).map(|_| ChannelValue::None).collect())
+
+    }
 }
