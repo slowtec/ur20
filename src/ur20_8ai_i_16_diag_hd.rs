3e21436197aff15f19578e9aee64a7de07c8e76f
Markus Kohlhase <markus.kohlhase@slowtec.de>
2018-03-16 12:32:29 +0100

    add method to build a UR20-8AI-I-16-DIAG-HD module from parameter data

--- src/ur20_8ai_i_16_diag_hd.rs
+++ src/ur20_8ai_i_16_diag_hd.rs
@@ -8,17 +8,27 @@ pub struct Mod {
     pub ch_params: Vec<ChannelParameters>,
 }
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq, Eq)]
 pub struct ModuleParameters {
     pub frequency_suppression: FrequencySuppression,
 }
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq, Eq)]
 pub struct ChannelParameters {
     pub channel_diagnostics: bool,
     pub diag_short_circuit: bool,
     pub data_format: DataFormat,
     pub measurement_range: AnalogIRange,
+}
+
+impl Mod {
+    pub fn from_parameter_data(data: &[u16]) -> Result<Mod> {
+        let (mod_params, ch_params) = parameters_from_raw_data(data)?;
+        Ok(Mod {
+            mod_params,
+            ch_params,
+        })
+    }
 }
 
 impl Default for ModuleParameters {
@@ -104,6 +114,63 @@ impl Module for Mod {
     }
 }
 
+fn parameters_from_raw_data(data: &[u16]) -> Result<(ModuleParameters, Vec<ChannelParameters>)> {
+    if data.len() < 33 {
+        return Err(Error::BufferLength);
+    }
+    let mut module_parameters = ModuleParameters::default();
+
+    module_parameters.frequency_suppression = match data[0] {
+        0 => FrequencySuppression::Disabled,
+        1 => FrequencySuppression::Hz50,
+        2 => FrequencySuppression::Hz60,
+        3 => FrequencySuppression::Average16,
+        _ => return Err(Error::ChannelParameter),
+    };
+
+    let channel_parameters: Result<Vec<_>> = (0..8)
+        .map(|i| {
+            let mut p = ChannelParameters::default();
+            let idx = i * 4;
+
+            p.channel_diagnostics = match data[idx + 1] {
+                0 => false,
+                1 => true,
+                _ => {
+                    return Err(Error::ChannelParameter);
+                }
+            };
+
+            p.diag_short_circuit = match data[idx + 2] {
+                0 => false,
+                1 => true,
+                _ => {
+                    return Err(Error::ChannelParameter);
+                }
+            };
+
+            p.data_format = match data[idx + 3] {
+                0 => DataFormat::S7,
+                1 => DataFormat::S5,
+                _ => {
+                    return Err(Error::ChannelParameter);
+                }
+            };
+
+            p.measurement_range = match data[i * 4 + 4] {
+                0 => AnalogIRange::mA0To20,
+                1 => AnalogIRange::mA4To20,
+                2 => AnalogIRange::Disabled,
+                _ => {
+                    return Err(Error::ChannelParameter);
+                }
+            };
+            Ok(p)
+        })
+        .collect();
+    Ok((module_parameters, channel_parameters?))
+}
+
 #[cfg(test)]
 mod tests {
 
@@ -167,5 +234,164 @@ mod tests {
                 .is_err()
         );
         assert_eq!(m.process_output_values(&[]).unwrap(), &[]);
+    }
+
+    #[test]
+    fn test_module_parameters_from_raw_data() {
+        let mut data = vec![
+            0,          // Module
+            0, 0, 0, 0, // CH 0
+            0, 0, 0, 0, // CH 1
+            0, 0, 0, 0, // CH 2
+            0, 0, 0, 0, // CH 3
+            0, 0, 0, 0, // CH 4
+            0, 0, 0, 0, // CH 5
+            0, 0, 0, 0, // CH 6
+            0, 0, 0, 0, // CH 7
+        ];
+
+        assert_eq!(
+            parameters_from_raw_data(&data)
+                .unwrap()
+                .0
+                .frequency_suppression,
+            FrequencySuppression::Disabled
+        );
+        data[0] = 1;
+        assert_eq!(
+            parameters_from_raw_data(&data)
+                .unwrap()
+                .0
+                .frequency_suppression,
+            FrequencySuppression::Hz50
+        );
+        data[0] = 2;
+        assert_eq!(
+            parameters_from_raw_data(&data)
+                .unwrap()
+                .0
+                .frequency_suppression,
+            FrequencySuppression::Hz60
+        );
+        data[0] = 3;
+        assert_eq!(
+            parameters_from_raw_data(&data)
+                .unwrap()
+                .0
+                .frequency_suppression,
+            FrequencySuppression::Average16
+        );
+    }
+
+    #[test]
+    fn test_channel_parameters_from_raw_data() {
+        let data = vec![
+            0,          // Module
+            0, 0, 0, 2, // CH 0
+            1, 0, 0, 2, // CH 1
+            0, 1, 0, 2, // CH 2
+            0, 0, 1, 2, // CH 3
+            0, 0, 0, 1, // CH 4
+            1, 1, 1, 0, // CH 5
+            0, 0, 0, 0, // CH 6
+            0, 0, 0, 0, // CH 7
+        ];
+
+        assert_eq!(parameters_from_raw_data(&data).unwrap().1.len(), 8);
+        assert_eq!(
+            parameters_from_raw_data(&data).unwrap().1[0],
+            ChannelParameters::default()
+        );
+
+        assert_eq!(
+            parameters_from_raw_data(&data).unwrap().1[1].channel_diagnostics,
+            true
+        );
+        assert_eq!(
+            parameters_from_raw_data(&data).unwrap().1[1].diag_short_circuit,
+            false
+        );
+        assert_eq!(
+            parameters_from_raw_data(&data).unwrap().1[1].data_format,
+            DataFormat::S7
+        );
+        assert_eq!(
+            parameters_from_raw_data(&data).unwrap().1[1].measurement_range,
+            AnalogIRange::Disabled
+        );
+
+        assert_eq!(
+            parameters_from_raw_data(&data).unwrap().1[2].diag_short_circuit,
+            true
+        );
+        assert_eq!(
+            parameters_from_raw_data(&data).unwrap().1[3].data_format,
+            DataFormat::S5
+        );
+        assert_eq!(
+            parameters_from_raw_data(&data).unwrap().1[4].measurement_range,
+            AnalogIRange::mA4To20
+        );
+    }
+
+    #[test]
+    fn test_parameters_from_invalid_raw_data() {
+        let mut data = vec![
+            0,          // Module
+            0, 0, 0, 0, // CH 0
+            0, 0, 0, 0, // CH 1
+            0, 0, 0, 0, // CH 2
+            0, 0, 0, 0, // CH 3
+            0, 0, 0, 0, // CH 4
+            0, 0, 0, 0, // CH 5
+            0, 0, 0, 0, // CH 6
+            0, 0, 0, 0, // CH 7
+        ];
+        data[1] = 2; // should be '0' or '1'
+        assert!(parameters_from_raw_data(&data).is_err());
+
+        data[1] = 0;
+        data[2] = 2; // should be '0' or '1'
+        assert!(parameters_from_raw_data(&data).is_err());
+
+        data[2] = 0;
+        data[3] = 2; // should be '0' or '1'
+        assert!(parameters_from_raw_data(&data).is_err());
+
+        data[3] = 0;
+        data[4] = 3; // should be '0','1' or '2'
+        assert!(parameters_from_raw_data(&data).is_err());
+    }
+
+    #[test]
+    fn test_parameters_from_invalid_data_buffer_size() {
+        let data = [0; 0];
+        assert!(parameters_from_raw_data(&data).is_err());
+        let data = [0; 32];
+        assert!(parameters_from_raw_data(&data).is_err());
+        let data = [0; 33];
+        assert!(parameters_from_raw_data(&data).is_ok());
+    }
+
+    #[test]
+    fn create_module_from_parameter_data() {
+        let data = vec![
+            0,          // Module
+            0, 0, 0, 0, // CH 0
+            0, 0, 0, 2, // CH 1
+            1, 0, 0, 0, // CH 2
+            0, 0, 0, 0, // CH 3
+            0, 0, 0, 0, // CH 4
+            0, 0, 0, 0, // CH 5
+            0, 0, 0, 0, // CH 6
+            0, 0, 0, 0, // CH 7
+        ];
+        let module = Mod::from_parameter_data(&data).unwrap();
+        assert_eq!(module.ch_params[0].measurement_range, AnalogIRange::mA0To20);
+        assert_eq!(
+            module.ch_params[1].measurement_range,
+            AnalogIRange::Disabled
+        );
+        assert_eq!(module.ch_params[2].channel_diagnostics, true);
     }
 }
